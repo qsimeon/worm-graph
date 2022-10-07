12cebf9d10de4871cf75e58b1e9f9a0879cf7e3b
gyyang <gyyang@users.noreply.github.com>
2022-10-07 15:36:08 -0400

    making main

---
+++ main.py
@@ -0,0 +1,51 @@
+from preprocessing import WormNeuralDynamicsDataLoader
+from preprocessing import *
+from torch_geometric_temporal.signal import temporal_signal_split
+
+try:
+    from tqdm import tqdm
+except ImportError:
+    def tqdm(iterable):
+        return iterable
+
+import torch
+import torch.nn.functional as F
+from torch_geometric_temporal.nn.recurrent import DCRNN
+
+
+# Load data
+loader = WormNeuralDynamicsDataLoader.WormNeuralDynamicsDataLoader()
+dataset = loader.get_dataset()
+train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
+
+
+# Create network
+model = RecurrentGCN(node_features=3)
+
+
+# Train
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+model.train()
+
+for epoch in tqdm(range(200)):
+    cost = 0
+    for time, snapshot in enumerate(train_dataset):
+        y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
+        cost = cost + torch.mean((y_hat - snapshot.y) ** 2)
+    cost = cost / (time + 1)
+    cost.backward()
+    optimizer.step()
+    optimizer.zero_grad()
+
+model.eval()
+cost = 0
+for time, snapshot in enumerate(test_dataset):
+    y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
+    cost = cost + torch.mean((y_hat - snapshot.y) ** 2)
+cost = cost / (time + 1)
+cost = cost.item()
+print("MSE: {:.4f}".format(cost))
+
+
+# Visualize
--- models/dcrnn.py
+++ models/dcrnn.py
@@ -1,21 +1,3 @@
-from preprocessing import *
-from preprocessing import WormNeuralDynamicsDataLoader
-from torch_geometric_temporal.signal import temporal_signal_split
-
-loader = WormNeuralDynamicsDataLoader.WormNeuralDynamicsDataLoader()
-dataset = loader.get_dataset()
-train_dataset, test_dataset = temporal_signal_split(dataset, train_ratio=0.2)
-
-try:
-    from tqdm import tqdm
-except ImportError:
-    def tqdm(iterable):
-        return iterable
-
-import torch
-import torch.nn.functional as F
-from torch_geometric_temporal.nn.recurrent import DCRNN
-
 class RecurrentGCN(torch.nn.Module):
     def __init__(self, node_features):
         super(RecurrentGCN, self).__init__()
@@ -27,31 +9,7 @@ class RecurrentGCN(torch.nn.Module):
         h = F.relu(h)
         h = self.linear(h)
         return h
-        
-model = RecurrentGCN(node_features = 3)
 
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
-model.train()
-
-for epoch in tqdm(range(200)):
-    cost = 0
-    for time, snapshot in enumerate(train_dataset):
-        y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
-        cost = cost + torch.mean((y_hat-snapshot.y)**2)
-    cost = cost / (time+1)
-    cost.backward()
-    optimizer.step()
-    optimizer.zero_grad()
-    
-model.eval()
-cost = 0
-for time, snapshot in enumerate(test_dataset):
-    y_hat = model(snapshot.x, snapshot.edge_index, snapshot.edge_attr)
-    cost = cost + torch.mean((y_hat-snapshot.y)**2)
-cost = cost / (time+1)
-cost = cost.item()
-print("MSE: {:.4f}".format(cost))
 
 
 
