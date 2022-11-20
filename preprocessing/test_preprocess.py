e9ac414aa85140b6c5705785f75662550f962866
qsimeon <qsimeon@mit.edu>
2022-11-19 21:00:53 -0500

    included 3 neural datasets

--- preprocessing/test_preprocess.py
+++
@@ -1,6 +0,0 @@
-import os
-from utils import ROOT_DIR
-
-pyfile = os.path.join(ROOT_DIR, 'preprocessing', 'process_raw.py')
-print(pyfile)
-assert os.path.exists(pyfile), "Preprocessing step not implemented!"
