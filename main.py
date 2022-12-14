993ae8b1966523ea9c85ffc3d30fa6b1afd94f37
qsimeon <qsimeon@mit.edu>
2022-12-13 19:05:39 -0500

    plot main.py

--- main.py
+++ main.py
@@ -1,30 +1,60 @@
-from data.load_connectome import CElegansDataset
-from data.load_neural_activity import load_Uzel2022
-from tasks.all_tasks import OneStepPrediction
-from models.gnn_models import EvolveRCGN
-from train.train_gnn import optimize_model
-from train.train_gnn import model_predict
-import matplotlib.pyplot as plt
+from data.load_neural_activity import pick_worm, load_dataset
+from visualization.plot_loss_log import plot_loss_log
+from visualization.plot_before_after_weights import plot_before_after_weights
+from visualization.plot_correlation_scatter import plot_correlation_scatter
+from visualization.plot_target_prediction import plot_target_prediction
+from models.linear_models import LinearNN
+from train.train_main import optimize_model
+from train.train_main import model_predict
+import numpy as np
 
 if __name__ == "__main__":
-    # laod conectome
-    graph = CElegansDataset()[0]
-    # load data
-    Uzel2022 = load_Uzel2022(); single_worm_dataset = Uzel2022['worm1']
-    dataset, neuron_ids = single_worm_dataset['data'], single_worm_dataset['neuron_ids']
-    # define the task
-    task = OneStepPrediction(graph, dataset, neuron_ids)
-    # construct the model
-    model = EvolveRCGN(node_count=graph.num_nodes, node_features=task.seq_len)
-    # train the model
-    model, log = optimize_model(task, model)
-    preds = model_predict(task, model)
-    # TODO: put plotting code in a function `inside train.py``
-    plt.figure()
-    plt.plot(log['epochs'], log['train_losses'], label='train')
-    plt.plot(log['epochs'], log['test_losses'], label='test')
-    plt.xlabel('Epoch')
-    plt.ylabel('MSE')
-    plt.title('EvolveRCGN loss curves')
-    plt.legend()
-    plt.show()
+    #@title Train a simple linear regression model.
+    # choose your dataset
+    dataset_name = 'Skora2018'
+    all_worms_dataset = load_dataset(dataset_name)
+    print('Chosen dataset:', dataset_name)
+    print()
+    # get data for one worm
+    worm = np.random.choice(list(all_worms_dataset.keys()))
+    single_worm_dataset = pick_worm(all_worms_dataset, worm)
+    print('Picked:', worm)
+    print()
+    # get the calcium data for this worm
+    calcium_data = single_worm_dataset['data']
+    print('worm calcium-signal dataset:', calcium_data.shape)
+    print()
+    # get the neuron ids and length of the recording
+    neuron_ids = single_worm_dataset['neuron_ids']
+    neuron_idx = np.random.choice(list(neuron_ids.keys()))-1
+    max_time = single_worm_dataset['max_time']
+    num_neurons = single_worm_dataset['num_neurons']
+    # create the model and place on GPU
+    lin_model = LinearNN(input_size=num_neurons).double()
+    # store the initial weights
+    untrained_weights = lin_model.linear.weight.detach().cpu()
+    # optimize the model (use default settings)
+    lin_model, log = optimize_model(dataset=calcium_data, 
+                                    model=lin_model, 
+                                    seq_len=range(1,14), num_epochs=500)
+    # get the trained weights 
+    trained_weights = lin_model.linear.weight.detach().cpu()
+    # plot loss curves
+    plot_loss_log(log, plt_title="%s, %s neurons, data size %s, seq. len %s \n "
+    "Linear Model: $\Delta Y(t) = W^{\intercal} Y(t)$ loss "
+    "curves"%(worm.upper(), num_neurons, log['data_size'], log['seq_len']))
+    # trained vs untrained weights
+    plot_before_after_weights(untrained_weights, trained_weights, W_name='W')
+    #@title Make predictions with the trained linear model.
+    nid = neuron_ids[neuron_idx]
+    targets, predictions = model_predict(single_worm_dataset, lin_model)
+    # plot prediction for a single neuron
+    plot_target_prediction(targets[:, neuron_idx], predictions[:, neuron_idx], 
+                        plt_title="%s, neuron %s, data size %s, seq. len %s \n "
+                        "Linear model: Ca2+ residuals prediction"%(
+                            worm.upper(), nid, log['data_size'], log['seq_len']))
+    # plot scatterplot of all neuron predictions
+    plot_correlation_scatter(targets, predictions, 
+                            plt_title="%s, %s neurons, data size %s, \n Linear "
+                            "Model: Correlation of all neuron Ca2+ residuals"%(
+                                worm.upper(), num_neurons, log['data_size']))
--- train/train_main.py
+++ train/train_main.py
@@ -126,30 +126,16 @@ def optimize_model(dataset, model, num_epochs=100, seq_len=1, data_size=1000):
   # return optimized model
   return model, log
 
-
-def lstm_hidden_size_experiment(dataset, num_epochs, input_size, num_layers=1, 
-                              hid_mult=np.array([3, 2]), seq_len=range(1,11,1)):
+def model_predict(single_worm_dataset, model):
   """
-  Helper function to experiment with different input sizes for the LSTM model.
-  dataset: the dataset to train on.
-  num_epochs: number of epochs to train for.
-  input_size: number of input features (neurons).
-  num_layers: number of hidden layers to use in the LSTM.
-  hid_mult: np.array of integers to multiple input_size by.
-  seq_len: array of sequnce lengths to train on.
+  Makes predictions for all neurons in the given
+  worm dataset using a trained model.
   """
-  hidden_experiment = dict()
-  # we experiment with different hidden sizes
-  for hidden_size in input_size*hid_mult:
-    hidden_size = int(hidden_size)
-    print()
-    print("Hidden size: %d" % hidden_size)
-    print("~~~~~~~~~~~~~~~")
-    # initialize model, optimizer and loss function
-    lstm_model = NetworkLSTM(input_size, hidden_size, num_layers).double() 
-    # optimize the model
-    lstm_model, log = optimize_model(dataset=dataset, model=lstm_model, 
-                                  num_epochs=num_epochs, seq_len=seq_len)
-    # log results of this experiment      
-    hidden_experiment[hidden_size] = log
-  return hidden_experiment
+  calcium_data = single_worm_dataset['data']
+  # model in/out
+  input = calcium_data.squeeze().to(device)
+  output = model(input)
+  # tagets/preds
+  targets = (input[1:] - input[:-1]).detach().cpu()
+  predictions = output[:-1].detach().cpu()
+  return targets, predictions
