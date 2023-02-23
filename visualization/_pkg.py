b51d4f6d1d03d2f68bc40d5158b6268d75fba033
qsimeon <qsimeon@mit.edu>
2023-02-23 13:08:28 -0500

    Update _pkg.py

--- visualization/_pkg.py
+++ visualization/_pkg.py
@@ -8,8 +8,7 @@ import numpy as np
 import networkx as nx
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-from omegaconf import DictConfig
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, DictConfig
 from matplotlib.lines import Line2D
 from data._utils import NeuralActivityDataset, BatchSampler
 from models._utils import *
