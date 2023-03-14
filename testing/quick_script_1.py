e493be220b2efc6aa036e95d0ce1a9d7e767e962
qsimeon <qsimeon@mit.edu>
2023-03-14 10:53:41 -0400

    moved quick scripts

---
+++ testing/quick_script_1.py
@@ -0,0 +1,40 @@
+"""
+Testing out prediction with untrained 
+model on data from a single worm.
+"""
+from models._utils import NeuralCFC
+
+from train._utils import model_predict
+
+from omegaconf import OmegaConf
+
+from data._main import get_dataset
+
+import matplotlib.pyplot as plt
+
+config = OmegaConf.load("conf/dataset.yaml")
+
+if __name__=="__main__":
+    dataset = get_dataset(config)
+
+    single_worm_dataset = dataset["worm0"]
+
+    calcium_data = single_worm_dataset["calcium_data"]
+
+    model = NeuralCFC(302, 64).double()
+
+    fig, ax = plt.subplots(1, 1)
+
+    ax.imshow(model.linear.weight.detach().cpu().T)
+
+    ax.set_title("Model readout weights")
+
+    ax.set_xlabel("Output size")
+
+    ax.set_ylabel("Input size")
+
+    plt.show()
+
+    targets, predictions = model_predict(model, calcium_data)
+
+    print("Targets:", targets.shape, "\nPredictions:", predictions.shape, end="\n\n")
