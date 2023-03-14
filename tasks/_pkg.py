2f087c896b19f9bada81da74ec08b0c960cc1c67
qsimeon <qsimeon@mit.edu>
2023-03-14 11:04:58 -0400

    moved quick scripts

--- tasks/_pkg.py
+++ tasks/_pkg.py
@@ -9,7 +9,3 @@ from data._utils import (
     CElegansConnectome,
     load_Uzel2022,
 )
-from torch_geometric_temporal.signal import temporal_signal_split
-from torch_geometric_temporal.signal.static_graph_temporal_signal import (
-    StaticGraphTemporalSignal,
-)
--- testing/quick_script_2.py
+++ testing/quick_script_2.py
@@ -1,3 +1,7 @@
+"""
+Tests the model optimization, the full training 
+pipeline and the train test masks.
+"""
 import matplotlib.pyplot as plt
 from omegaconf import OmegaConf
 from models._utils import LinearNN
@@ -6,30 +10,28 @@ from data._main import get_dataset
 from train._utils import optimize_model
 from visualization._utils import plot_before_after_weights
 
+config = OmegaConf.load("conf/dataset.yaml")
+
 if __name__ == "__main__":
-    # load dataset and get calcium data for one worm
-    dataset = get_dataset(OmegaConf.load("conf/dataset.yaml"))
-    calcium_data = dataset["worm0"]["calcium_data"]
+    # load a dataset (multiple worms)
+    dataset = get_dataset(config)
+    # get calcium data for one worm
+    single_worm_dataset = dataset["worm0"]
+    calcium_data = dataset["calcium_data"]
     # create a model
     model = LinearNN(302, 64).double()
-    # train the model
+    # test the  `optimize_model` function
     kwargs = dict(train_size=4096, test_size=4096, tau=1, seq_len=47, reverse=False)
     model, log = optimize_model(calcium_data, model, k_splits=2, **kwargs)
-
-    plt.figure()
-
-    plt.plot(log["train_mask"].to(float).numpy())
-
-    plt.title("Train mask")
-
-    plt.xlabel("Time")
-
-    plt.ylabel("Test (0) / Train (1)")
-
-    plt.show()
-
+    # run the full train pipeline
     config = OmegaConf.load("conf/train.yaml")
-
     model, log_dir = train_model(model, dataset, config, shuffle=True)
-
+    # plot figure showing train mask
+    plt.figure()
+    plt.plot(log["train_mask"].to(float).numpy())
+    plt.title("Train mask")
+    plt.xlabel("Time")
+    plt.ylabel("Test (0) / Train (1)")
+    plt.show()
+    # plot untrained versus trained weights
     plot_before_after_weights(log_dir)
