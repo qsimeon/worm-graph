fbe8351f0b37b84c7327c435e149c527a5e9922a
ivyy-zhao <ivyivyzhao77@gmail.com>
2023-03-21 23:17:01 -0400

    gov func & coeff on polyorder 1

--- govfunc/_utils.py
+++ govfunc/_utils.py
@@ -207,28 +207,28 @@ def coef_analysis(worm_name, n_cluster):
         plt.show()
         return dendrogram["ivl"], dendrogram["leaves_color_list"]
 
-    def plot_dendrogram_agg(model, w, **kwargs):
-        # Create linkage matrix and then plot the dendrogram
-
-        # create the counts of samples under each node
-        counts = np.zeros(model.children_.shape[0])
-        n_samples = len(model.labels_)
-        for i, merge in enumerate(model.children_):
-            current_count = 0
-            for child_idx in merge:
-                if child_idx < n_samples:
-                    current_count += 1  # leaf node
-                else:
-                    current_count += counts[child_idx - n_samples]
-            counts[i] = current_count
-
-        linkage_matrix = np.column_stack(
-            [model.children_, model.distances_, counts]
-        ).astype(float)
-        # Plot the corresponding dendrogram
-        dendrogram(linkage_matrix, **kwargs)
-        plt.title("dendrogram of " + w)
-        plt.savefig('./govfunc/coefficient/dendrogram_' + w + '.png')
+    # def plot_dendrogram_agg(model, w, **kwargs):
+    #     # Create linkage matrix and then plot the dendrogram
+    #
+    #     # create the counts of samples under each node
+    #     counts = np.zeros(model.children_.shape[0])
+    #     n_samples = len(model.labels_)
+    #     for i, merge in enumerate(model.children_):
+    #         current_count = 0
+    #         for child_idx in merge:
+    #             if child_idx < n_samples:
+    #                 current_count += 1  # leaf node
+    #             else:
+    #                 current_count += counts[child_idx - n_samples]
+    #         counts[i] = current_count
+    #
+    #     linkage_matrix = np.column_stack(
+    #         [model.children_, model.distances_, counts]
+    #     ).astype(float)
+    #     # Plot the corresponding dendrogram
+    #     dendrogram(linkage_matrix, **kwargs)
+    #     plt.title("dendrogram of " + w)
+    #     plt.savefig('./govfunc/coefficient/dendrogram_' + w + '.png')
 
     def main_function(w, n):
         ###############################
@@ -260,7 +260,7 @@ def coef_analysis(worm_name, n_cluster):
 
         agg = AgglomerativeClustering(n_clusters=n, metric="precomputed", linkage="complete", compute_distances=True)
         agg.fit(distance)
-        plot_dendrogram_agg(agg, w, truncate_mode="level", p=n)
+        # plot_dendrogram_agg(agg, w, truncate_mode="level", p=n)
         label_col = np.array(agg.labels_, dtype=int)
         col_category = np.stack((index_col, label_col)).T
 
