829a07717a8e739516775d94da2d2180d23edee2
ivyy-zhao <ivyivyzhao77@gmail.com>
2023-03-24 11:34:08 -0400

    val_loss

--- testing/ivy_scripts/quick_scripts/test.py
+++ testing/ivy_scripts/quick_scripts/test.py
@@ -2,7 +2,7 @@ import os
 import hydra
 from omegaconf import OmegaConf, DictConfig
 
-@hydra.main(version_base=None, config_path="../conf", config_name="testing")
+@hydra.main(version_base=None, config_path="../../conf", config_name="testing")
 def main(cfg: DictConfig) -> None:
     print("Working directory:", os.getcwd(), end="\n\n")
     print("`config` YAML:\n", OmegaConf.to_yaml(cfg), end="\n\n")
