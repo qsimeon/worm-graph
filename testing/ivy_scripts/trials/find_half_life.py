d059504ede114187d940db803bb918c5099f4bc1
ivyy-zhao <ivyivyzhao77@gmail.com>
2023-03-27 18:45:09 -0400

    find half life

--- testing/ivy_scripts/trials/find_half_life.py
+++ testing/ivy_scripts/trials/find_half_life.py
@@ -40,10 +40,10 @@ if __name__ == "__main__":
     neuron_range = [12, 22, 59, name_mask]
 
     # different choices for tau
-    # half_life_seq_len_range = [1]
+    half_life_seq_len_range = range(1, 300, 2)
     # # different choices for seq_len
-    seq_len_range = range(1, 100, 2)
-    epoch = 1
+    # seq_len_range = range(1, 100, 2)
+    epoch = 100
 
     list_save = []
     for no in neuron_range:
@@ -55,48 +55,47 @@ if __name__ == "__main__":
             residual = single_worm_dataset["residual_calcium"][:, no].to(
                 torch.float32)
 
-
         alpha_list = []
-        for seq_len in seq_len_range:
+        for half_seq_len in half_life_seq_len_range:
             # initialize network hyperparameters
-            net = Net(1)
-            optimizer = torch.optim.SGD(net.parameters(), lr=1)
+            net = Net(half_seq_len)
+            optimizer = torch.optim.SGD(net.parameters(), lr=0.001)
             loss_func = torch.nn.MSELoss()
 
-            input_group = []
-            target_group = []
-            for t in range(seq_len, residual.shape[0] - 1):
-                cal_input = residual[t - seq_len:t].T
-                target = residual[t:t + 1].T.squeeze()
-                input_group.append(np.array(cal_input))
-                target_group.append(np.array(target))
-            train_dataset = torch.utils.data.TensorDataset(torch.tensor(np.array(input_group)),
-                                                           torch.tensor(np.array(target_group)))
-            train_loader = torch.utils.data.DataLoader(
-                dataset=train_dataset,
-                batch_size=128,
-                shuffle=True,
-            )
+            for length in range(1, residual.shape[0] - 1):
+                input_group = []
+                target_group = []
+                for t in range(length, residual.shape[0] - 1):
+                    cal_input = residual[t - length:t].T
+                    target = residual[t:t + 1].T.squeeze()
+                    input_group.append(np.array(cal_input))
+                    target_group.append(np.array(target))
+                train_dataset = torch.utils.data.TensorDataset(torch.tensor(np.array(input_group)),
+                                                               torch.tensor(np.array(target_group)))
+                train_loader = torch.utils.data.DataLoader(
+                    dataset=train_dataset,
+                    batch_size=128,
+                    shuffle=True,
+                )
 
-
-            # prepare batches of samples
-            for e in range(epoch):
-                for cal_input, target in train_loader:
-                    prediction = net.forward(cal_input).squeeze()
-                    loss = loss_func(prediction, target)
-                    optimizer.zero_grad()
-                    loss.backward()
-                    optimizer.step()
-            print("seq_len = ", seq_len, "alpha = ", net.state_dict()["alpha"].item())
+                # prepare batches of samples
+                for e in range(epoch):
+                    for cal_input, target in train_loader:
+                        prediction = net.forward(cal_input).squeeze()
+                        loss = loss_func(prediction, target)
+                        optimizer.zero_grad()
+                        loss.backward()
+                        optimizer.step()
+            print("half_seq_len = ", half_seq_len, "alpha = ", net.state_dict()["alpha"].item())
             alpha_list.append(net.state_dict()["alpha"].item())
         # for each neuron: for each tau model had an optimal alpha
         list_save.append(alpha_list)
 
     for k in range(len(list_save)):
         if k == len(list_save) - 1:
-            plt.plot(seq_len_range, list_save[k], linewidth=2)
+            plt.plot(half_life_seq_len_range, list_save[k], linewidth=2)
         else:
-            plt.plot(seq_len_range, list_save[k])
+            plt.plot(half_life_seq_len_range, list_save[k])
 
     print(list_save[-1][-1])
 
@@ -104,7 +103,7 @@ if __name__ == "__main__":
     list_name = [single_worm_dataset["slot_to_neuron"][i] for i in neuron_range]
     list_name.append("all neurons")
     plt.legend(list_name)
-    plt.title("alpha - seq_len_range")
-    plt.xlabel("seq_len_range, epoch = 1, tau = 1")
+    plt.title("alpha - half_seq_len_range")
+    plt.xlabel("half_seq_len_range, epoch = 100")
     plt.ylabel("optimal solution of alpha")
     plt.show()
