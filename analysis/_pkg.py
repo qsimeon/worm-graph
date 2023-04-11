9e24bfdc97623e8c4cf677c5594652f7eec9eae9
ivyy-zhao <ivyivyzhao77@gmail.com>
2023-04-11 18:20:22 -0400

    neuron_on_category

--- analysis/_pkg.py
+++ analysis/_pkg.py
@@ -5,3 +5,7 @@ import itertools
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from collections import defaultdict
+from utils import ROOT_DIR
+from torch_geometric.data import Data
+import datetime
--- analysis/_utils.py
+++ analysis/_utils.py
@@ -3,6 +3,7 @@
 
 from analysis._pkg import *
 from visualization._utils import *
+import utils
 
 def find_config_files(root_dir):
     for file in os.listdir(root_dir):
@@ -130,3 +131,118 @@ def plot_loss_vs_parameter_sorted(configs, param_names):
     plt.savefig(os.path.join(os.getcwd()+"/analysis/figures/", "val_loss_on_[" + str(param_names[0]) + "_" + str(time_stamp) + "].png"))
     plt.show()
 
+
+
+def neurons_on_category(model, dataset, smooth_data, tau_out):
+    '''
+    predict neuron activities using pre-trained model
+
+    Args:
+        model: pre-trained model
+        dataset: whole dataset (multi worms)
+        smooth_data: Boolean, choose whether to use smoothed data or not
+        tau_out: offset to the future prediction
+    Returns:
+        Tuple, (worm_name, {category1: {max, min, mean}, category2: {max, min, mean}, .....})
+        the result losses(max, min, average) of all worms with categorized neurons
+    Figures:
+        save the figures about the loss based on neuron category, across worms
+    '''
+
+
+    neuron_type = ["inter", "motor", "other", "pharynx", "sensory", "sexspec"]
+
+    color_list = []
+
+    model.eval()
+
+    # load the category of neurons: len(graph_tensors) == 302
+    graph_tensors = torch.load(os.path.join(ROOT_DIR, "data/processed/connectome", "graph_tensors.pt"))
+    graph = Data(**graph_tensors)
+
+    # TODO: add criterion as one of the hyperparameters in this function
+    criterion = torch.nn.L1Loss()
+
+    multi_worm_category_loss = dict()
+    for i in range(len(dataset)):
+        single_worm_dataset = dataset["worm" + str(i)]
+        if smooth_data:
+            calcium_data = single_worm_dataset["smooth_calcium_data"]
+        else:
+            calcium_data = single_worm_dataset["calcium_data"]
+        max_time = single_worm_dataset["max_time"]
+        named_neuron_mask = single_worm_dataset["named_neurons_mask"]
+
+        named_neuron_inds = torch.where(named_neuron_mask)[0].numpy()
+
+        # get the type of named neurons
+        category_neuron = [graph.y[item].item() for item in named_neuron_inds]
+
+        targets, predictions = model_predict(model, calcium_data[:max_time//2, :] * named_neurons_mask, tau=tau_out)
+        loss_prediction = [criterion(predictions[:, idx], targets[:, idx]).detach().item() for idx in named_neuron_inds]
+        # print(len(named_neuron_inds), len(loss_prediction))
+
+        # named_neuron_category_loss is a dict
+        # key: (neuron_inds, loss)
+        # val: neuron_category
+        named_neuron_category_loss = dict()
+        for k in range(len(named_neuron_inds)):
+            named_neuron_category_loss.setdefault(
+                (named_neuron_inds[k], loss_prediction[k]),
+                category_neuron[k]
+            )
+
+
+        # reverse_dict is a dict
+        # key: neuron_category
+        # val: (neuron_inds, loss)
+        reverse_dict = defaultdict(list)
+        for key, val in sorted(named_neuron_category_loss.items()):
+            reverse_dict[val].append(key)
+
+        one_worm_category_neuron_loss = dict()
+        for res_catagory in reverse_dict.items():
+            loss_list = [loss for (neuron, loss) in res_catagory[1]]
+            max_loss = np.array(loss_list).max()
+            min_loss = np.array(loss_list).min()
+            avg_loss = np.array(loss_list).mean()
+            one_worm_category_neuron_loss.setdefault(
+                res_catagory[0],
+                {
+                    "avg_loss": avg_loss,
+                    "max_loss": max_loss,
+                    "min_loss": min_loss,
+                }
+            )
+
+        multi_worm_category_loss["worm" + str(i)] = one_worm_category_neuron_loss
+
+    # plot the average & max & min loss of each worm with categorized neurons
+    for worm in multi_worm_category_loss.items():
+        sorted_category = sorted(list(worm[1]))
+        min_loss_list = [worm[1][idx]["min_loss"] for idx in sorted_category]
+        max_loss_list = [worm[1][idx]["max_loss"] for idx in sorted_category]
+        avg_loss_list = [worm[1][idx]["avg_loss"] for idx in sorted_category]
+
+        sns.lineplot(
+            x=sorted_category,
+            y=avg_loss_list,
+            label="avg. " + worm[0],
+            alpha=0.5,
+        )
+
+        plt.gca().fill_between(
+            sorted_category,
+            min_loss_list,
+            max_loss_list,
+            alpha=0.1,
+            # label=worm[0],
+        )
+    plt.xticks(range(6), neuron_type)
+
+    plt.ylabel("categorized prediction loss")
+    plt.xlabel("category of neurons")
+    plt.title("Analysis on neuron-type and prediction-loss \n Dataset: " + dataset["worm0"]["dataset"])
+    plt.savefig(os.path.join(ROOT_DIR, "analysis", "figures", "category_" + datetime.now().strftime("%Y_%m_%d_%H_%M_%S") + ".png"))
+
+    return multi_worm_category_loss
