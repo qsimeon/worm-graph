db64bb50341cac3644be9effc55087a7eb16a6db
qsimeon <qsimeon@mit.edu>
2023-04-11 21:33:06 -0400

    delete .sh files and add to git ignore

--- conda.sh
+++
@@ -1,7 +0,0 @@
-# wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh
-# mkdir /om2/user/kyzhao/mnconda
-# sh Miniconda3-py39_4.9.2-Linux-x86_64.sh -f -p /om2/user/kyzhao/mnconda
-
-source activate /om2/user/kyzhao/worm_conda/envs/worm-graph
-conda activate /om2/user/kyzhao/worm_conda/envs/worm-graph
-source activate /om2/user/kyzhao/worm_conda/envs/worm-graph
--- init.sh
+++
@@ -1,1 +0,0 @@
-srun -n 4 -t 12:00:00 --gres=gpu:2 --pty bash
