cac5057c21919f88ffc540357aeb6bbd67c047e3
qsimeon <qsimeon@mit.edu>
2023-04-30 16:02:16 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ import pandas as pd
 os.environ["HYDRA_FULL_ERROR"] = "1"
 os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 os.environ["OC_CAUSE"] = "1"
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:32"
 
 NUM_NEURONS = 302
 
