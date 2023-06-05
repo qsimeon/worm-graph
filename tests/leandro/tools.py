4a07dd62ffea68c0f86917b37673c28964c0c0bb
Leandro R. Venancio <rissov.leandro@gmail.com>
2023-06-05 10:19:12 -0400

    New threshold for correlation

--- tests/leandro/tools.py
+++ tests/leandro/tools.py
@@ -9,3 +9,29 @@ def correlation_matrix(data):
 def is_symmetric(matrix):
     transpose = np.transpose(matrix)
     return np.array_equal(matrix, transpose)
+
+def surrogate_test(data, num_surrogates=1000):
+    # Step 1: Calculate observed correlation matrix
+    observed_corr = np.corrcoef(data, rowvar=False)
+
+    # Step 2: Generate surrogate datasets
+    surrogates = []
+    for _ in range(num_surrogates):
+        surrogate_data = data.copy()
+        np.random.shuffle(surrogate_data.T) # Shuffle each column
+        surrogates.append(surrogate_data)
+
+    # Step 3: Calculate correlation matrices for surrogates
+    surrogate_corrs = []
+    for surrogate in surrogates:
+        surrogate_corr = np.corrcoef(surrogate, rowvar=False)
+        surrogate_corrs.append(surrogate_corr)
+
+    # Step 4: Compare observed correlations to surrogates
+    surrogate_corrs = np.array(surrogate_corrs)
+    surrogate_summary = np.mean(surrogate_corrs, axis=0)  # Can also use np.median or other summary statistics
+
+    # Step 5: Assess statistical significance
+    corr_threshold = np.mean(surrogate_summary)
+
+    return observed_corr, surrogate_summary, corr_threshold
