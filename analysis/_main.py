e7288303071daf9d994ae9e0e704a7faab00f4d5
Leandro R. Venancio <rissov.leandro@gmail.com>
2023-06-30 17:09:04 -0400

    start moving hc to analysis

--- analysis/_main.py
+++ analysis/_main.py
@@ -15,30 +15,10 @@ def analysis(
             # Do something with the data
             configs[os.path.dirname(file_path)] = OmegaConf.create(data)
 
-    # import pdb; pdb.set_trace()
-    # print(get_config_value(configs['../logs/hydra/2023_03_30_16_59_28'], 'preprocess.smooth'))
-    # print(get_config_value(configs['../logs/hydra/2023_03_30_16_59_28'], 'model.type'))
-
-    # print(os.getcwd())
-    # print(type(configs), len(configs))
-
-    # d = configs['logs/hydra/2023_04_03_20_53_10/dataset.name=Uzel2022,globals.shuffle=True,train.epochs=50']
-    # print(type(d))
-    # print(OmegaConf.to_yaml(d))
-
-    plot_loss_vs_parameter_sorted(configs, param_names=config.analysis.param_names)
-
-    # plotting predictions
-    for log_dir in configs:
-        with open(os.path.join(log_dir + "/config.yaml"), "r") as f:
-            yaml_file = yaml.safe_load(f)
-            plot_targets_predictions(
-                log_dir,
-                "worm0",
-                "AVAL",
-                get_config_value(yaml_file, "global.use_residuals"),
-            )
-
+    # === Hierarchical clustering ===
+    # Random pick a dataset
+    dataset_names = [ds_name for ds_name in configs[config.analysis.dir]['dataset']['name'].split('_')]
+    hierarchical_clustering_algorithm(dataset_names, criterion_value=4, show_plots=True, distance='cosine')
 
 if __name__ == "__main__":
     config = OmegaConf.load("conf/analysis.yaml")
--- analysis/_pkg.py
+++ analysis/_pkg.py
@@ -11,3 +11,12 @@ from omegaconf import DictConfig, OmegaConf
 from statsmodels.nonparametric.smoothers_lowess import lowess
 from torch_geometric.data import Data
 from utils import ROOT_DIR
+
+# === Hierarchical clustering ===
+import json
+from scipy.cluster.hierarchy import dendrogram, linkage, fcluster
+from scipy.spatial.distance import squareform
+from dtaidistance import dtw
+import sklearn.metrics as sm
+from visualize._utils import plot_heat_map
+from data._main import get_dataset
