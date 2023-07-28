8655355ee5b5352726ef7b82049f7df513cb2b80
Leandro R. Venancio <rissov.leandro@gmail.com>
2023-07-28 11:32:35 -0400

    save

--- main.py
+++ main.py
@@ -1,90 +1,7 @@
 from pkg import *
 
-
-@hydra.main(version_base=None, config_path="conf")
-def pipeline(
-    config: DictConfig,
-) -> None:
-    """Runs a complete pipeline to train a model and make predictions.
-
-    Can be configured using the main.yaml file in the conf directory.
-
-    Parameters
-    ----------
-    config: DictConfig
-        Hydra configuration object.
-
-    Calls
-    -----
-    process_data : function in preprocess/_main.py
-        Configuration file in conf/preprocess.yaml
-
-    get_dataset : function in datasets/_main.py
-        Configuration file in conf/dataset.yaml
-
-    get_model : function in models/_main.py
-        Configuration file in conf/model.yaml
-
-    train_model : function in train/_main.py
-        Configuration file in conf/train.yaml
-
-    plot_figures : function in visualization/_main.py
-        Configuration file in conf/visualize.yaml
-
-    TODO: Implement `analyze_outputs` : function in analysis/_main.py; config in conf/analysis.yaml
-
-    Notes
-    -----
-    * Use mode: RUN if you are having a UserWarning with MULTIRUN
-
-    """
-    # Display Pytorch device
-    torch_device()
-
-    # Intialize random seeds
-    init_random_seeds(config.globals.random_seed)
-
-    # Skips if data already preprocessed
-    #! process_data(config)
-
-    # Returns a generator of single worm datasets
-    dataset = get_dataset(config)
-
-    # Get the model to train
-    model = get_model(config)
-
-    # Train model is the bulk of the pipeline code
-    model, log_dir, config = train_model(
-        config,
-        model,
-        dataset,
-        shuffle_worms=config.globals.shuffle_worms,  # shuffle worms
-        log_dir=None,  # hydra changes working directory to log directory
-    )
-
-    # Use trained model to make predictions on the dataset
-    make_predictions(
-        config,  # `train_model` injected the `predict` params into config`
-        model=None,
-        dataset=None,
-        log_dir=log_dir,
-        use_residual=config.globals.use_residual,
-        smooth_data=config.globals.smooth_data,
-    )
-
-    # Plot figures
-    plot_figures(config, log_dir)
-
-    ## TODO: Analysis of outputs
-    # analyze_outputs(config, log_dir)
-
-    # Free up GPU
-    clear_cache()
-
-    return None
-
 @hydra.main(version_base=None, config_path="conf", config_name="pipeline")
-def my_app(cfg: DictConfig) -> None:
+def pipeline(cfg: DictConfig) -> None:
     """Create a custom pipeline"""
 
     print(OmegaConf.to_yaml(cfg), end="\n\n")
@@ -106,20 +23,21 @@ def my_app(cfg: DictConfig) -> None:
              assert 'train' in cfg.submodule, "Train must be defined before visualizing (or chose a log_dir)."
              assert 'predict' in cfg.submodule, "Predict must be defined before visualizing (or chose a log_dir)."
 
-    # Print all submodules to be included in the pipeline
-    #print(OmegaConf.to_yaml(cfg.submodule))
+    torch_device() # Display Pytorch device
+
+    init_random_seeds(cfg.seed) # Intialize random seeds
 
     if 'preprocess' in cfg.submodule:
-        process_data(cfg.submodule.preprocess) # working
+        process_data(cfg.submodule.preprocess)
     
     if 'dataset_train' in cfg.submodule:
-        dataset_train = get_dataset(cfg.submodule.dataset_train) # working
+        dataset_train = get_dataset(cfg.submodule.dataset_train)
 
     if 'dataset_predict' in cfg.submodule:
-        dataset_predict = get_dataset(cfg.submodule.dataset_predict) # working
+        dataset_predict = get_dataset(cfg.submodule.dataset_predict)
 
     if 'model' in cfg.submodule:
-        model = get_model(cfg.submodule.model) # working
+        model = get_model(cfg.submodule.model)
 
     if 'train' in cfg.submodule:
         model, submodules_updated, train_info = train_model(
@@ -155,6 +73,7 @@ def my_app(cfg: DictConfig) -> None:
     if 'analysis' in cfg.submodule:
         print(cfg.submodule.analysis)
 
+    clear_cache() # Free up GPU
+
 if __name__ == "__main__":
-    #! pipeline()
-    my_app()
+    pipeline()
