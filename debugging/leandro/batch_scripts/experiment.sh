1db94bfc3142b58e8396051ddcfaba6a1870e5a0
Leandro Risso Venancio <lrvenan@mit.edu>
2023-08-04 16:39:36 -0400

    save

---
+++ debugging/leandro/batch_scripts/experiment.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SBATCH --job-name=experiment
+SBATCH --output=experiment_out.txt
+
+
+#SBATCH --job-name=my_job
+#SBATCH --output=res.txt
+#SBATCH --ntasks=1
+#SBATCH --time=10:00
+#SBATCH --mem-per-cpu=100
+
+module add openmind/anaconda/3-2022.05
+srun conda activate worm-graph
