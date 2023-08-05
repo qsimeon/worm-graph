755da036dfe8faceb5b35dad8da1f9ec32e4c0fd
Leandro R. Venancio <rissov.leandro@gmail.com>
2023-08-04 23:53:12 -0400

    save

--- debugging/leandro/batch_scripts/experiment.sh
+++ debugging/leandro/batch_scripts/experiment.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-SBATCH --job-name=experiment
-SBATCH --output=experiment_out.txt
-SBATCH --gres=gpu:A100:2
-SBATCH --mem=64000
-SBATCH --time=12:00:00
-
-#SBATCH --job-name=my_job
-#SBATCH --output=res.txt
-#SBATCH --ntasks=1
-#SBATCH --time=10:00
-#SBATCH --mem-per-cpu=100
+#SBATCH --job-name=experiment
+#SBATCH --output=experiment_out.txt
+#SBATCH --gres=gpu:A100:2
+#SBATCH --mem=64000
+#SBATCH --time=12:00:00
 
 module add openmind/anaconda/3-2022.05
 srun hostname
