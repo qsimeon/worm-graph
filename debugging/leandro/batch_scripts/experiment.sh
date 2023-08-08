a91ae9b03f9474e148ce02a0273e4b042db71b99
Leandro R. Venancio <rissov.leandro@gmail.com>
2023-08-08 09:35:21 -0400

    save

--- debugging/leandro/batch_scripts/experiment.sh
+++ debugging/leandro/batch_scripts/experiment.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=experiment
-#SBATCH --output=experiment_out.txt
-#SBATCH --gres=gpu:A100:2
+#SBATCH --output=/om2/user/lrvenan/worm-graph/debugging/leandro/batch_scripts/experiment_out.txt
+#SBATCH --gres=gpu:2
 #SBATCH --mem=64000
 #SBATCH --time=12:00:00
 
-module add openmind/anaconda/3-2022.05
-srun hostname
+module load openmind/anaconda/3-2022.05
 srun conda activate worm-graph
-srun python main.py +experiment=default_run
+srun python main.py +submodule=preprocess
