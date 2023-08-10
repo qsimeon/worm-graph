320e26148b76969c1d6968bafcc95133d5bc261e
Leandro Risso Venancio <lrvenan@mit.edu>
2023-08-10 19:34:24 -0400

    save

--- debugging/leandro/batch_scripts/hyp_search.sh
+++ debugging/leandro/batch_scripts/hyp_search.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=search_transformer
-#SBATCH --output=/om2/user/lrvenan/worm-graph/debugging/leandro/batch_scripts/search_transformer.txt
-#SBATCH --gres=gpu:2
-#SBATCH --mem=64000
-#SBATCH --time=12:00:00
+#SBATCH --job-name=search_lstm
+#SBATCH --output=/om2/user/lrvenan/worm-graph/debugging/leandro/batch_scripts/search_lstm.txt
+#SBATCH --gres=gpu:4
+#SBATCH --mem=32000
+#SBATCH --time=24:00:00
 
 cd /om2/user/lrvenan/worm-graph
 source /om2/user/lrvenan/miniconda/bin/activate worm-graph
-srun python main.py +experiment=hyperparameter_transformer
+srun python main.py +experiment=hyperparameter_lstm
