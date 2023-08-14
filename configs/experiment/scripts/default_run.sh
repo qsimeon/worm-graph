bbe8a708a7ba89efd1f71d1ccdd3085416c81afe
Leandro Risso Venancio <lrvenan@mit.edu>
2023-08-14 14:39:51 -0400

    configuring experiment scripts

--- configs/experiment/scripts/default_run.sh
+++ configs/experiment/scripts/default_run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+#SBATCH --job-name=main
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --time=1440
+#SBATCH --cpus-per-task=1
+#SBATCH --gres=gpu:1
+#SBATCH --gpus-per-node=1
+#SBATCH --partition=yanglab
+
+#SBATCH --error=configs/experiment/scripts/default_run.err
+#SBATCH --output=configs/experiment/scripts/default_run.out
+
 cd /om2/user/lrvenan/worm-graph
 source /om2/user/lrvenan/miniconda/bin/activate worm-graph
 srun python main.py +experiment=default_run
