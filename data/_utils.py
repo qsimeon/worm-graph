fixed calculate distance computation!

data/_utils.py
@@ -638,17 +638,18 @@ def select_desired_worms(multi_worm_dataset, worms):
     # worms can be str, list or int
     if isinstance(worms, str):
         # User requested one specific worm
-        logger.info("\tUsing {} from {}".format(worms, dataset_name))
+        logger.info(f"Using {worms} from {dataset_name}.")
         wormIDs_to_keep = [worms]
     elif isinstance(worms, int):
         # User requested a specific number of worms (random pick)
         if worms > len(multi_worm_dataset):
             logger.info(
-                f"Requested number of worms was more than the number of worms in {dataset_name}. Defaulting to using all {len(multi_worm_dataset)} worms in {dataset_name}."
+                f"Requested number of worms was more than the number of worms in {dataset_name}. "
+                f"Defaulting to using all {len(multi_worm_dataset)} worms in {dataset_name}."
             )
             worms = len(multi_worm_dataset)
         wormIDs_to_keep = np.random.choice(wormIDs, size=worms, replace=False)
-        logger.info("\tUsing {} worms from {} (random pick)".format(worms, dataset_name))
+        logger.info(f"Using {worms} worms from {dataset_name} (random pick).")
     elif isinstance(worms, list):
         # User requested specific worms
         if len(worms) > len(multi_worm_dataset):
@@ -657,7 +658,7 @@ def select_desired_worms(multi_worm_dataset, worms):
             )
             worms = list(multi_worm_dataset.keys())
         wormIDs_to_keep = worms
-        logger.info("\tUsing {} from {}".format(wormIDs_to_keep, dataset_name))
+        logger.info(f"Using {wormIDs_to_keep} from {dataset_name}.")
     else:
         raise Exception("Invalid type for `worms` argument.")
 

models/_utils.py
@@ -644,41 +644,38 @@ class Model(torch.nn.Module):
         masked_neural_sequence = neural_sequence * feature_mask.unsqueeze(
             1
         )  # (batch_size, seq_len, input_size) * (batch_size, 1, input_size) -> (batch_size, seq_len, input_size)
-
-        # ### >>> FAST but potentially INCORRECT, NEW Implementation >>> ###
-        # # NOTE: What makes this implementation potentially incorrect is that it does not apply the mask to the token_matrix;
-        # # therefore, the distance between each neural vector and codebook vector is not being computed only at masked positions.
-        # # Fast and memory efficient distance calculation
-        # flatten = masked_neural_sequence.view(
-        #     -1, input_size
-        # )  # (batch_size, seq_len, input_size) -> (batch_size * seq_len, input_size)
-        # matrix = token_matrix.t()  # (num_tokens, input_size) -> (input_size, num_tokens)
-        # distances = (
-        #     flatten.pow(2).sum(dim=1, keepdim=True)  # (batch_size * seq_len, 1)
-        #     - 2 * flatten @ matrix  # (batch_size * seq_len, num_tokens)
-        #     + matrix.pow(2).sum(0, keepdim=True)  # (1, num_tokens)
-        # )  # (batch_size * seq_len, num_tokens)
-        # distances = distances.view(batch_size, seq_len, -1)  # (batch_size, seq_len, num_tokens)
-        # ### <<< FAST but potentially INCORRECT, NEW Implementation <<< ###
-
-        ### >>> SLOW and CORRECT, ORIGINAL Implementation >>> ###
         # Applying the feature mask to the token matrix
         masked_token_matrix = token_matrix.unsqueeze(0) * feature_mask.unsqueeze(
             1
         )  # (1, num_tokens, input_size) * (batch_size, 1, input_size) -> (batch_size, num_tokens, input_size)
-        # Expand dimensions for broadcasting
-        masked_neural_sequence_expanded = masked_neural_sequence.unsqueeze(
-            2
-        )  # (batch_size, seq_len, 1, input_size)
-        masked_token_matrix_expanded = masked_token_matrix.unsqueeze(
-            1
-        )  # (batch_size, 1, num_tokens, input_size)
-        distances = torch.mean(
-            (masked_neural_sequence_expanded - masked_token_matrix_expanded)
-            ** 2,  # (batch_size, seq_len, 1, input_size) - (batch_size, 1, num_tokens, input_size) -> (batch_size, seq_len, num_tokens, input_size)
-            dim=-1,
+
+        ### >>> FAST, NEW Implementation >>> ###
+        # Fast and memory efficient distance calculation
+        TERM_1 = masked_neural_sequence.pow(2).sum(dim=-1, keepdim=True)  # (batch_size, seq_len, 1)
+        TERM_2 = -2 * torch.bmm(
+            masked_neural_sequence,
+            masked_token_matrix.permute(0, 2, 1),
         )  # (batch_size, seq_len, num_tokens)
-        ### <<< SLOW and CORRECT, ORIGINAL Implementation <<< ###
+        TERM_3 = (
+            masked_token_matrix.pow(2).sum(dim=-1, keepdim=True).permute(0, 2, 1)
+        )  # (batch_size, 1, num_tokens)
+        distances = TERM_1 + TERM_2 + TERM_3  # (batch_size, seq_len, num_tokens)
+        ### <<< FAST, NEW Implementation <<< ###
+
+        # ### >>> SLOW, ORIGINAL Implementation >>> ###
+        # # Expand dimensions for broadcasting
+        # masked_neural_sequence_expanded = masked_neural_sequence.unsqueeze(
+        #     2
+        # )  # (batch_size, seq_len, 1, input_size)
+        # masked_token_matrix_expanded = masked_token_matrix.unsqueeze(
+        #     1
+        # )  # (batch_size, 1, num_tokens, input_size)
+        # distances = torch.mean(
+        #     (masked_neural_sequence_expanded - masked_token_matrix_expanded)
+        #     ** 2,  # (batch_size, seq_len, 1, input_size) - (batch_size, 1, num_tokens, input_size) -> (batch_size, seq_len, num_tokens, input_size)
+        #     dim=-1,
+        # )  # (batch_size, seq_len, num_tokens)
+        # ### <<< SLOW, ORIGINAL Implementation <<< ###
 
         # Return distance matrix
         return distances